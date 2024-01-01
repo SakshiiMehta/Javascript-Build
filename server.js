93d9f3e6db2abf8d4ec6294f80161050e3313d32
unknown <sakshidmehta2003@gmail.com>
2024-01-01 23:33:11 +0530

    Express Updates

--- server.js
+++ server.js
@@ -1,102 +1,41 @@
-const http = require("http");
+const express = require("express");
+
+const app = express (); // Initialising express
+app.use(express.json()); //to make application work in json format
 
 const port = 8081; // local port num
 
-// HTTP Methods
-
-/*
->> GET: Inorder to get data from server
->> POST: Sending data to server
->> DELETE: Deleting the data from database
->> PATCH: Updating certain fields
->> PUT: Full Update
-*/
 
 const toDoList = ["learn", "apply things", "succed"];
 
-http
-  .createServer((req, res) => {
-    // call back func
-    const { method, url } = req;
+// http://localhost:8081/todos
+app.get("/todos",(req,res)=>{
+  res.status(200 ).send(toDoList)
 
-    // console.log(method, url);
 
-    if (url === "/todos") {
-      if (method === "GET") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(toDoList.toString());
-      } else if (method === "POST") {
-        let body = "";
-        req
-          .on("error", (err) => {
-            console.log(err);
-          })
-          .on("data", (chunk) => {
-            body += chunk;
-            // console.log(chunk);
-          })
-          .on("end", () => {
-            body = JSON.parse(body);
+});
 
-            let newToDo = toDoList;
-            newToDo.push(body.devtown);
-            console.log(newToDo);
-            // console.log("data: ", body);
-          });
-      } else if (method === "DELETE") {
-        let body = "";
-        req
-          .on("error", (err) => {
-            console.error(err);
-          })
-          .on("data", (chunk) => {
-            body += chunk;
-          })
-          .on("end", () => {
-            body = JSON.parse(body);
+app.post("/todos",(req,res)=>{
+  let newToDoItem = req.body.name;
+  toDoList.push(newToDoItem);
+  res.status(201).send({message: "Task Added Successfully"});
+});
 
-            let deleteThisItem = body.item;
+app.delete("/todos",(req,res)=>{
+  const deleteThisItem = req.body.name;
 
-            for (let i = 0; i < toDoList.length; i++) {
-              if (toDoList[i] === deleteThisItem) {
-                toDoList.splice(i, 1);
-                break;
-              } else {
-                console.error("Error: Match Not Found!!");
-                break;
-              }
-            }
-
-            // toDoList.find((elem, index) => {
-            //   if (elem === deleteThisItem) {
-            //     toDoList.splice(index, 1);
-            //   } else {
-            //     console.error("Error: Match Not Found!!");
-            //     // console.exit();
-            //   }
-            // });
-          });
-      } else {
-        res.writeHead(501);
-      }
-    } else {
-      res.writeHead(404);
+  toDoList.find((elem,index)=>{
+    if (elem === deleteThisItem){
+      toDoList.splice(index,1);
     }
-    res.end();
-    // res.writeHead(200, { "Content-Type": "text/html" });
-    // res.write("<h2>hey server started n u can procced :-) 123456 </h2>");
-    // res.end();
-  })
-  .listen(port, () => {
-    // call back func
-    console.log(`NodeJs Server Started Running on Port: ${port}`);
-  });
+    res.status(202).send({message: `Deleted item ${req.body.name}`});
+  }); 
+});
 
-// http:localhost:8081
-// http:localhost:8081/
+app.all("/todos" ,(req,res)=>{
+ res.status(501).send();
+});
 
-// http://localhost:8081/signin
-// http://localhost:8081/signup
-// http://localhost:8081/home
-// http://localhost:8081/contactUs
-// http://localhost:8081/AboutUs
+app.listen (port,()=>{
+  console.log(`Nodejs Server started running on Port $(port)`);
+});
